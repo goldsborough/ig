5741ec3301d16ff085681ce917243234b92ea0af
goldsborough <peter@goldsborough.me>
2017-05-09 11:58:53 +0100

    Bumped version

--- ig/__init__.py
+++ ig/__init__.py
@@ -2,7 +2,7 @@ from datetime import date
 
 __title__ = 'ig'
 __url__ = "https://github.com/goldsborough/ig"
-__version__ = '0.1.5'
+__version__ = '0.1.6'
 __author__ = 'Peter Goldsborough'
 __license__ = 'MIT'
 __copyright__ = 'Copyright {0} Peter Goldsborough'.format(date.today().year)
